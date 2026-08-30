6b38f968deb9864dc5467151a8af0cb96a28d9fb
11maznsabry11 <111mazensabry111@gmail.com>
2026-08-30 21:43:18 +0300

    chore: update devDependencies and enhance type definitions
    - Added Playwright and Testing Library packages for improved testing capabilities.
    - Updated role type in index.d.ts from "family" to "parent" for better clarity.
    - Introduced new course and level types to enhance type safety.

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,9 +1,13 @@
 import { AxiosError } from "axios";
 
 export type TGender = "male" | "female";
-export type TRole = "family" | "student" | "teacher";
+
+export type TRole = "parent" | "student" | "teacher";
+
 export type TUserSearchType = "familes" | "students" | "teachers";
+
 export type TCourseType = "private" | "online" | "general";
+
 export type TLevel = "beginner" | "intermediate" | "advanced" | "expert";
 
 export type AxiosResErr = AxiosError<{ message: string }>;
